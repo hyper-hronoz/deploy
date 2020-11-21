6ea057a21cd021cb07c356a71821590842e30c2c
Vladislav <vlad_moryakov_prog@mail.ru>
2020-11-21 22:56:34 +0300

    m

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 const app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname)));
 const port = process.env.PORT || 80;
 app.listen(port);
